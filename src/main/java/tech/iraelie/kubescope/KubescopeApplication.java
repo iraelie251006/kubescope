54e73e2568534be4783860bf1e94b051b09cb836
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-04 23:29:21 +0200

    Initialized KubescopeApplication

---
+++ src/main/java/tech/iraelie/kubescope/KubescopeApplication.java
@@ -0,0 +1,13 @@
+package tech.iraelie.kubescope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubescopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubescopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/iraelie/kubescope/KubescopeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.iraelie.kubescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubescopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
