ebe97a98a58dd803d27024f87b11cd924f773765
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-05 23:12:29 +0200

    enabled @EnableJpaAuditing

--- src/main/java/tech/iraelie/kubescope/config/JpaAuditingConfig.java
+++ src/main/java/tech/iraelie/kubescope/config/JpaAuditingConfig.java
@@ -1,7 +1,9 @@
 package tech.iraelie.kubescope.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class JpaAuditingConfig {
 }
