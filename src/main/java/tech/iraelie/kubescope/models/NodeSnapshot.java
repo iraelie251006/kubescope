3fe2e154d53f7ae31a07ebe5cbd9dcd34cf4e4aa
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-06 23:43:32 +0200

    implemented and designed structure for NodeSnapshot entity model

--- src/main/java/tech/iraelie/kubescope/models/NodeSnapshot.java
+++ src/main/java/tech/iraelie/kubescope/models/NodeSnapshot.java
@@ -1,9 +1,25 @@
 package tech.iraelie.kubescope.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Entity
 @Table(name = "node_snapshot")
 public class NodeSnapshot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "node_name", nullable = false)
+    private String nodeName;
+
+    private String instanceType;
+    private String region;
+
+    private Long cpuCapacityMillicores;
+    private Long memoryCapacityBytes;
+    private Long memoryUsageBytes;
+    private BigDecimal hourlyCostUsd;
 }
