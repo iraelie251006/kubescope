8e1f0914fca440da5054515e6f9f4de433ffefaf
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-06 23:49:15 +0200

    renamed table column names through @Column annotation

--- src/main/java/tech/iraelie/kubescope/models/NodeSnapshot.java
+++ src/main/java/tech/iraelie/kubescope/models/NodeSnapshot.java
@@ -15,11 +15,23 @@ public class NodeSnapshot {
     @Column(name = "node_name", nullable = false)
     private String nodeName;
 
+    @Column(name = "instance_type")
     private String instanceType;
+
     private String region;
 
+    @Column(name = "cpu_capacity_millicores")
     private Long cpuCapacityMillicores;
+
+    @Column(name = "cpu_usage_millicores")
+    private Long cpuUsageMillicores;
+
+    @Column(name = "memory_capacity_bytes")
     private Long memoryCapacityBytes;
+
+    @Column(name = "memory_usage_bytes")
     private Long memoryUsageBytes;
+
+    @Column(name = "hourly_cost_usd", precision = 10, scale = 4)
     private BigDecimal hourlyCostUsd;
 }
