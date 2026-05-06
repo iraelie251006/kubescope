b580870c9762d919b2e060da7f8daafccd2e2634
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-06 23:37:22 +0200

    renamed table name to node_snapshot

--- src/main/java/tech/iraelie/kubescope/models/NodeSnapshot.java
+++ src/main/java/tech/iraelie/kubescope/models/NodeSnapshot.java
@@ -1,7 +1,9 @@
 package tech.iraelie.kubescope.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "node_snapshot")
 public class NodeSnapshot {
 }
