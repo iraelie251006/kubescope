06c3f39033c6c866cd2bed4d5bacb3ad555714a9
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-07 23:38:14 +0200

    created a pod snapshot table for storing metadata regarding a kubernetes pod in the cluster

--- src/main/java/tech/iraelie/kubescope/models/PodSnapshot.java
+++ src/main/java/tech/iraelie/kubescope/models/PodSnapshot.java
@@ -1,7 +1,46 @@
 package tech.iraelie.kubescope.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "pod_snapshot")
 public class PodSnapshot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "pod_name", nullable = false)
+    private String podName;
+
+    @Column(nullable = false)
+    private String namespace;
+
+    @Column(name = "node_name")
+    private String nodeName;
+
+    @Column(name = "cpu_request_millicores")
+    private Long cpuRequestMillicores;
+
+    @Column(name = "memory_request_bytes")
+    private Long memoryRequestBytes;
+
+    @Column(name = "cpu_usage_millicores")
+    private Long cpuUsageMillicores;
+
+    @Column(name = "memory_usage_bytes")
+    private Long memoryUsageBytes;
+
+    private String phase;
+
+    @Column(nullable = false)
+    private Instant timestamp;
 }
