92479929d2cb0083e59c06ebbc961ea916a1bfee
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-07 23:58:46 +0200

    implemented NodeSnapshotRepository for database layer interaction with Node snashot entity

---
+++ src/main/java/tech/iraelie/kubescope/models/NodeSnapshotRepository.java
@@ -0,0 +1,20 @@
+package tech.iraelie.kubescope.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public interface NodeSnapshotRepository extends JpaRepository<NodeSnapshot, UUID> {
+
+    @Query("""
+            SELECT n FROM NodeSnapshot n
+            WHERE n.timestamp = (SELECT MAX(n2.timestamp) FROM NodeSnapshot n2)
+            """)
+    List<NodeSnapshot> findLatestSnapshot();
+
+    List<NodeSnapshot> findByTimestampGreaterThanEqualOrderByTimestampDesc(Instant since);
+}
+
