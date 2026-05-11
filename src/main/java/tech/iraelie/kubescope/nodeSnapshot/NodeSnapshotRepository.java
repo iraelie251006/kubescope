28a9d4c5853d76455418b1e8be6b3d39802fdfcd
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-11 23:50:01 +0200

    refactored NodeSnapshot and NodeSnapshotRepository to dedicated package nodeSnapshot

--- src/main/java/tech/iraelie/kubescope/nodeSnapshot/NodeSnapshotRepository.java
+++ src/main/java/tech/iraelie/kubescope/nodeSnapshot/NodeSnapshotRepository.java
@@ -1,4 +1,4 @@
-package tech.iraelie.kubescope.models;
+package tech.iraelie.kubescope.nodeSnapshot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
