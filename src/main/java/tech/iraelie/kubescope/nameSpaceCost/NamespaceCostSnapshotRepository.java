fed1312d5f5cb2721a29adb2c6460147a777e509
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-12 23:36:45 +0200

    constructed a query to find by timestamp greater than equal order by timestamp in descending order.

--- src/main/java/tech/iraelie/kubescope/nameSpaceCost/NamespaceCostSnapshotRepository.java
+++ src/main/java/tech/iraelie/kubescope/nameSpaceCost/NamespaceCostSnapshotRepository.java
@@ -2,7 +2,10 @@ package tech.iraelie.kubescope.nameSpaceCost;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 public interface NamespaceCostSnapshotRepository extends JpaRepository<NamespaceCostSnapshot, UUID> {
+    List<NamespaceCostSnapshot> findByTimestampGreaterThanEqualOrderByTimestampDesc(Instant since);
 }
