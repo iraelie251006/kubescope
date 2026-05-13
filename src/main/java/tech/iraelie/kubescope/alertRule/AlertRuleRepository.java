7637ff7add6eaf2e80959c05e33095aa1f644d65
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-13 21:01:56 +0200

    feat(alert): add AlertRuleRepository for managing alert rule data access

---
+++ src/main/java/tech/iraelie/kubescope/alertRule/AlertRuleRepository.java
@@ -0,0 +1,8 @@
+package tech.iraelie.kubescope.alertRule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AlertRuleRepository extends JpaRepository<AlertRule, UUID> {
+}
