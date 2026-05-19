40e8e360f9e38bb1279c153318382c045c643524
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-19 23:49:36 +0200

    Added missing kubernetes related imports like ApiClient, AppsV1Api, CoreV1Api, KubeConfig, ClientBuilder, and Config.

--- src/main/java/tech/iraelie/kubescope/config/KubernetesConfig.java
+++ src/main/java/tech/iraelie/kubescope/config/KubernetesConfig.java
@@ -1,5 +1,12 @@
 package tech.iraelie.kubescope.config;
 
+import io.kubernetes.client.Metrics;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.apis.AppsV1Api;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.KubeConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
