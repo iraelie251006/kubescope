7341da1bf2f3c4704565bb720e1498af14771add
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-19 23:58:12 +0200

    Added missing UserRepository interface and User entity in AuthService class for authentication and authorization mechanism

--- src/main/java/tech/iraelie/kubescope/security/AuthService.java
+++ src/main/java/tech/iraelie/kubescope/security/AuthService.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tech.iraelie.kubescope.domain.user.User;
+import tech.iraelie.kubescope.domain.user.UserRepository;
 
 
 @Slf4j
