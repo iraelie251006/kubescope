3d6d1bbca51b1ddc7022463c459afa1eb4c4b7d2
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-22 23:37:55 +0200

    feat(exceptions): added TokenException for invalid or expired tokens

---
+++ src/main/java/tech/iraelie/kubescope/security/exception/TokenException.java
@@ -0,0 +1,7 @@
+package tech.iraelie.kubescope.security.exception;
+
+public class TokenException extends RuntimeException {
+    public TokenException(String message) {
+        super(message);
+    }
+}
