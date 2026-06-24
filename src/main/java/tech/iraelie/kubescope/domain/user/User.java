7e4ef6ae842a18a7f6d72bd88461a9c22db0f4d3
Elie <niyubwayoiraelie5777@gmail.com>
2026-06-24 23:56:05 +0200

    Updated and Fixed mismatch in users table with migration files.

--- src/main/java/tech/iraelie/kubescope/domain/user/User.java
+++ src/main/java/tech/iraelie/kubescope/domain/user/User.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "app_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
